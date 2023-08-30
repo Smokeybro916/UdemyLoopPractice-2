bef340f8d1afdacea2a75ccaf337b7634a1eab42
Smokeybro916 <kyleluangasa@gmail.com>
2023-08-29 21:24:20 -0400

    second commit

--- practice.js
+++ practice.js
@@ -4,5 +4,11 @@ const people = ["Kyle", "Aki", "Bryan", "Jerm", "Matt"];
 for (let i = 0; i < people.length; i++) {
     console.log(people[i].toUpperCase());
 }
-
 /* This example is to show how to run a loop over an array */
+/* adding a while loop*/
+let count = 0;
+while (count <10) {
+    count++;
+    console.log(count)
+}
+ /* while loop will keep running until it prints 10, as the loop is true until then */
