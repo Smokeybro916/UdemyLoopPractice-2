cf5557781fee83882ba3ea8b48df0f744c407453
Smokeybro916 <kyleluangasa@gmail.com>
2023-08-29 20:39:02 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,8 @@
+const people = ["Kyle", "Aki", "Bryan", "Jerm", "Matt"]; 
+
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
+
+/* This example is to show how to run a loop over an array */
